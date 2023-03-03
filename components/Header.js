f2e8d0c16048141a8e3e15f2cfc5221fce42c160
Lyrockx <lyrockx@gmail.com>
2023-03-03 01:13:06 -0500

    a

--- components/Header.js
+++ components/Header.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function Header({ name }) {
   return (
     <header className="pt-20 pb-12">
-    <img src="https://github.com/Lyrockx/Image/blob/main/IMG_9521%202.jpeg?raw=true" alt="image description" className="w-12 h-12 rounded-full block mx-auto mb-4" />
+    <img src="https://github.com/Lyrockx/Image/blob/main/IMG_9521%202.jpeg?raw=true" alt="image description" className="w-20 h-20 rounded-full block mx-auto mb-4" />
     <p className="text-2xl dark:text-white text-center">
       <Link href="/">
         <a>{name}</a>
